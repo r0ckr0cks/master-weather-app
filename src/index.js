55cccf5dd7993ed6f0756650c34c1bfb83606fb6
r0ckr0cks <104546408+r0ckr0cks@users.noreply.github.com>
2022-07-03 22:22:26 -0400

    Show current temperature and wind speed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-//alert("Hello Panda");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "0055be677334d96da6935e298ba23ddb";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
